290158beb25cd3a666d331ae43c53702de60c4a1
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-08-14 19:04:13 -0600

    Attempting to make my task manager folder readable in github

---
+++ task_manager/app/controllers/tasks_controller.rb
@@ -0,0 +1,27 @@
+# app/controllers/tasks_controller.rb
+
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+  end
+
+  def create
+    binding.pry
+    task = Task.new({
+      title: params[:task][:title],
+      description: params[:task][:description]
+      })
+    binding.pry
+    task.save
+    binding.pry
+    redirect_to '/tasks'
+  end
+
+  def show
+    binding.pry
+    @task = Task.find(params[:id])
+  end
+end
